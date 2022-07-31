4326a19491f2f6403c8bcf88dacb0c20d64afeac
ataturan <ata.turan@gmail.com>
2022-07-31 19:59:36 +0300

    fix typo

--- src/ProxyPattern/DatabaseQueryExecuterImpl.java
+++ src/ProxyPattern/DatabaseQueryExecuterImpl.java
@@ -1,6 +1,6 @@
 package ProxyPattern;
 
-public class DatabaseQueryQueryExecuterImpl implements DatabaseQueryExecuter {
+public class DatabaseQueryExecuterImpl implements DatabaseQueryExecuter {
     @Override
     public void executeQuery(String query) throws Exception {
         System.out.println("Going to execute query => " + query);
--- src/ProxyPattern/DatabaseQueryExecuterProxy.java
+++ src/ProxyPattern/DatabaseQueryExecuterProxy.java
@@ -3,13 +3,13 @@ package ProxyPattern;
 public class DatabaseQueryExecuterProxy implements DatabaseQueryExecuter{
 
         boolean isAdmin;
-        DatabaseQueryQueryExecuterImpl executer;
+        DatabaseQueryExecuterImpl executer;
 
         public DatabaseQueryExecuterProxy(String name, String password){
             if(name.equals("Admin") && password.equals("Admin1234")){
                 isAdmin=true;
             }
-            executer = new DatabaseQueryQueryExecuterImpl();
+            executer = new DatabaseQueryExecuterImpl();
         }
 
     @Override
